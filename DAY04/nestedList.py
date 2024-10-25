d9907a6375e2f72051252b41945339423fada0b5
Abrar Zahin <abrar.bauet@gmail.com>
2024-10-26 00:47:46 +0600

    first commit

--- DAY04/nestedList.py
+++ DAY04/nestedList.py
@@ -2,19 +2,30 @@
 # SOLUTIONS
 
 
-input_num = int(input("Enter the list range value: "))
-empty_list = []
-
-for x in range(1, input_num + 1):
+list_range = int(input())
+nested_list = []
+score_list = []
+for x in range(1, list_range+1):
     name = input("Enter the name: ")
     score = float(input("Enter the score: "))
     
-    # Append each entry as a dictionary to the list
-    empty_list.append({name,score})
+    nested_list.append([name,score])
+    score_list.append(score)
+#     name_list.append(name)
+print(nested_list)
+print(score_list)
+# print(name_list)
 
-# Sort the list based on the "Score" key in descending order
-empty_list.sort(key=lambda x: x["Score"], reverse=True)
+sroted_score_list = list(set(score_list))
+sroted_score_list = sorted(sroted_score_list)
+secound_highest_val = sroted_score_list[1]
+print(secound_highest_val)
 
-print("Sorted List in Descending Order:")
-for item in empty_list:
-    print(f"Name: {item['Name']}, Score: {item['Score']}")
+name_list = []
+for students in nested_list:
+    if students[1] == secound_highest_val:
+        name_list.append(students[0])
+sorted_name_list = sorted(name_list)
+
+for student_name in sorted_name_list:
+    print(student_name)
